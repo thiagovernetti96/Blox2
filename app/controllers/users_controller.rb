9f81d2e517b1ec492ef9b9d1fd7af8e0c92faf86
thiago vernetti <thiago_vernetti@hotmail.com>
2020-12-24 14:30:00 -0300

    users controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+    def show
+        @user = User.find(params[:id])
+      end
+    
+      def edit
+        @user = current_user
+      end
+    
+      def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        redirect_to profile_path(@user), notice: "Infos updated"
+      end
+    
+      def user_params
+        params.require(:user).permit(:username,:full_name, :genre)
+      end
+    end
 end
