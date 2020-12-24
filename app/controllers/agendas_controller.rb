5276b007a8f1f78ed800d7e5337f0c0baa53ffd5
thiago vernetti <thiago_vernetti@hotmail.com>
2020-12-24 15:39:28 -0300

    routes created

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,2 +1,9 @@
 class AgendasController < ApplicationController
+    def create
+        @agenda = Agenda.new
+        @agenda.user = current_user
+        @agenda.activity = Activity.find(params[:activity_id])
+        @agenda.save
+        redirect_to activity_path(@exploration.activity)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[show edit update] do
+  resources :salas, only: %i[index show new create update] do
+  resources :agendas, only: %i[create]
+  end
 end
