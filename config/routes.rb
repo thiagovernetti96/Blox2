a10f8daf60051020932f7cda5ae8c6dcc033470f
thiago vernetti <thiago_vernetti@hotmail.com>
2020-12-24 16:08:32 -0300

    application html erb created

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   get 'salas/controller'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   resources :users, only: %i[show edit update] do
   resources :salas, only: %i[index show new create update] do
   resources :agendas, only: %i[create]
   end
+
+end
 end
