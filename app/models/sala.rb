9e51065194c2551d95d0a0dfa54c29637ea98cc7
thiago vernetti <thiago_vernetti@hotmail.com>
2020-12-24 14:10:33 -0300

    sala validations and associations

--- app/models/sala.rb
+++ app/models/sala.rb
@@ -1,2 +1,7 @@
 class Sala < ApplicationRecord
+    belongs_to :user
+    has_one :user
+    validates :title, presence: true
+    validades :initial_date, presence: true, length: {8..18}
+    validates :final_date, presence: true, lenght: {8..18}
 end
