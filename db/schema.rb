ed67172f10d4572cc2ae3a068ccb710f80916457
thiago vernetti <thiago_vernetti@hotmail.com>
2020-12-24 14:28:28 -0300

    users controller migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_165250) do
+ActiveRecord::Schema.define(version: 2020_12_24_171108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "agendas", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "salas", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
